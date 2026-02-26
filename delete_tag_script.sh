fc0df1f86a60749abbc52831f7c8c80513dba739
valentinab25 <@users.noreply.github.com>
2026-02-26 18:10:04 +0200

    fix delete tag script

--- delete_tag_script.sh
+++ delete_tag_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 
- if [ -z "$SONARQUBE_TAG" ] || [ -z "$SONAR_HOST_URL" ] || [ -z "$SONARQUBE_TOKEN" ];
+ if [ -z "$SONARQUBE_TAG" ] || [ -z "$SONAR_HOST_URL" ] || [ -z "$SONARQUBE_TOKEN" ]; then
 	 echo "NEED VARIABLES"
 	 exit 1
  fi
@@ -8,7 +8,17 @@
  #SONARQUBE_TAG=demo-kitkat.dev2aws.eea.europa.eu
 
  project_result=$(curl -s "${SONAR_HOST_URL}api/components/search_projects?filter=tags%20%3D%20$SONARQUBE_TAG" )
- for i in $(echo -e "$project_result" | jq -r '.components[].name'); do tags=$(echo -e "$project_result" | jq -r ".components[] | select (.name == \"$i\") | .tags[]" | tr '\n' ',' | sed "s/$SONARQUBE_TAG,//"); curl -XPOST  -u "${SONARQUBE_TOKEN}:" "${SONAR_HOST_URL}api/project_tags/set?project=$i&tags=$tags"; echo $i; echo $tags; echo "--------------------"; done
+
+ echo "$project_result" 
+
+ echo "------"
+ for i in $(echo  "$project_result" | jq -r '.components[].name'); do 
+	 echo $i
+	 tags=$(echo "$project_result" | jq -r ".components[] | select (.name == \"$i\") | .tags[]" | tr '\n' ',' | sed "s/$SONARQUBE_TAG,//")
+	 curl -XPOST  -u "${SONARQUBE_TOKEN}:" "${SONAR_HOST_URL}api/project_tags/set?project=$i&tags=$tags"
+	 echo $tags
+	 echo "--------------------"
+ done
  
 
 
